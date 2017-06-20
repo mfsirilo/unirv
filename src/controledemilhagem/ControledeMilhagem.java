91f9d087a7ba51d0b7c7c92494fdb5a634698150
marcos <marcos flavio>
2017-06-20 19:47:43 -0300

    INICIO DE TUDO

---
+++ src/controledemilhagem/ControledeMilhagem.java
@@ -0,0 +1,61 @@
+/*
+ Controle de milhagem
+Uma CIA. aérea deseja controlar a pontuação do seu programa de milhagens usando
+arquivos e ArrayLists. Os clientes cadastrados são mantidos em uma arraylist 
+gravada em um arquivo, assim como os voos. As milhas são calculadas com base 
+na distancia convertida e arrendodada para cima (ex: 100km -> 62,10mi -> 63milhas).
+Clientes categoria outro ganham 1,5x mais milhas por viagem que clientes normais. (prata)
+e clientes diamante ganham o dobro. O histporico de voos de cada cliente é mantido em um arquivo cujo
+nome é formado pela primeiro nome do cliente + seu código. Deve ser inplementado um programa que 
+permita:
+
+- cadastro de clientes, voos e voo por cliente;
+- Exibição de histórico de voo por cliente
+- Exibição de saldo de milhas individuais 
+- Exibição de saldo de milhas familiar (pessoa + conjuge, se existir)
+obs: o sistema deve possuir tela de cadastros e deve mostrar o codigo e as 
+informaççoes dos cliente nas telas de exibição de dados
+
+
+voos
+- codigo 
+- origem 
+- destino
+- distancia
+
+voocliente
+- codigo cliente
+- codigo voo
+- data
+- hora
+
+clientes 
+- codigo
+- nome
+- sexo
+- cpf
+- categoria
+-- prata x1 milhas
+-- ouro x1,5 milhas
+-- diamante x2 milhas
+- coidgoConjuge - codigo marido / mulher
+-- -1 se solteiro / outros
+
+
+ */
+package controledemilhagem;
+
+/**
+ *
+ * @author marcos
+ */
+public class ControledeMilhagem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
