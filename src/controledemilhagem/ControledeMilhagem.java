9d5c4ed6f08baee21f3d7f516bc68cb99ad262aa
marcos <marcos flavio>
2017-06-21 22:28:06 -0300

    Commit inical do projeto

--- src/controledemilhagem/ControledeMilhagem.java
+++ src/controledemilhagem/ControledeMilhagem.java
@@ -1,11 +1,48 @@
 package controledemilhagem;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.*;
 import java.text.*;
 
 public class ControledeMilhagem {
+    
+    public static ArrayList saidaArquivo(ArrayList arraylist, String enderecoArquivo) {
+
+        try {
+            FileOutputStream saida = new FileOutputStream(enderecoArquivo);
+            ObjectOutputStream escritorObjetos = new ObjectOutputStream(saida);
+            escritorObjetos.writeObject(arraylist);
+            saida.close();
+            System.out.println("Arquivo Gravado!!");
+            return arraylist;
+        } catch (Exception e) {
+            System.out.println("ERRO!");
+            return arraylist;
+        }
+    }
+
+    public static ArrayList entradaArquivo(ArrayList arraylist, String enderecoArquivo) {
+
+        try {
+            InputStream entrada = new FileInputStream(enderecoArquivo);
+            ObjectInputStream leitorObjetos = new ObjectInputStream(entrada);
+            arraylist = (ArrayList) leitorObjetos.readObject();
+            entrada.close();
+            return arraylist;
+        } catch (Exception e) {
+            System.out.println("Erro");
+            return arraylist;
+        }
+    }
+
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
        
         
         
