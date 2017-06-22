fbbab74ef3f50e9cf616c345363b44a9d69dc9b4
marcos <marcos flavio>
2017-06-21 23:06:51 -0300

    Commit inical do projeto

--- src/controledemilhagem/Metodos.java
+++ src/controledemilhagem/Metodos.java
@@ -4,5 +4,13 @@ import java.io.*;
 import java.util.*;
 
 public class Metodos {
+    
+    public static void CadastroCliente (ArrayList <Clientes> listaclientes){
+        
+    }
+    
+    public static void CadastroVoo (ArrayList<Voos> listavoo){
+        
+    }
 
 }
